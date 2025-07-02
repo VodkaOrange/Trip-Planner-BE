37f68d01abc11dba24b30bd458c2cfd54c1e7205
kkondov <Kaloyan.Kondov.extern@dertouristik.com>
2025-07-02 09:59:35 +0200

    add a canFitAnotherActivityInTheSameDay field to DayPlanDto

--- src/main/java/com/tripplanner/dto/DayPlanDto.java
+++ src/main/java/com/tripplanner/dto/DayPlanDto.java
@@ -1,5 +1,6 @@
 package com.tripplanner.dto;
 
+import com.tripplanner.entity.Activity;
 import com.tripplanner.entity.DayPlan;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,18 +9,21 @@ public class DayPlanDto {
     private Long id;
     private int dayNumber;
     private List<ActivityDto> activities;
+    private Boolean canFitAnotherActivityInTheSameDay;
 
-    public DayPlanDto(Long id, int dayNumber, List<ActivityDto> activities) {
+    public DayPlanDto(Long id, int dayNumber, List<ActivityDto> activities, Boolean canFitAnotherActivityInTheSameDay) {
         this.id = id;
         this.dayNumber = dayNumber;
         this.activities = activities;
+        this.canFitAnotherActivityInTheSameDay = canFitAnotherActivityInTheSameDay;
     }
 
     public static DayPlanDto fromEntity(DayPlan dayPlan) {
         return new DayPlanDto(
                 dayPlan.getId(),
                 dayPlan.getDayNumber(),
-                dayPlan.getActivities().stream().map(ActivityDto::fromEntity).collect(Collectors.toList())
+                dayPlan.getActivities().stream().map(ActivityDto::fromEntity).collect(Collectors.toList()),
+                dayPlan.getActivities().stream().mapToDouble(Activity::getExpectedDurationHours).sum() < 10
         );
     }
 
@@ -45,5 +49,15 @@ public class DayPlanDto {
 
     public void setActivities(List<ActivityDto> activities) {
         this.activities = activities;
+    }
+
+    public Boolean getCanFitAnotherActivityInTheSameDay() {
+
+        return canFitAnotherActivityInTheSameDay;
+    }
+
+    public void setCanFitAnotherActivityInTheSameDay(Boolean canFitAnotherActivityInTheSameDay) {
+
+        this.canFitAnotherActivityInTheSameDay = canFitAnotherActivityInTheSameDay;
     }
 }
